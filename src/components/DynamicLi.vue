ca4f2d391fec0b8ae0b8e1cea61b11616ad1f6dc
MirkoFarina <MirkoFarina@outlook.com>
2022-11-21 12:50:25 +0100

    [change] milestone 1

---
+++ src/components/DynamicLi.vue
@@ -0,0 +1,33 @@
+<script>
+export default {
+    name:'DynamicLi',
+    props: {
+        titolo: String,
+        titoloOriginal: String,
+        lingua: String,
+        voto: Number
+    }
+}
+</script>
+
+
+<template>
+        <li>
+            <h3>
+                titolo: {{titolo}}
+            </h3>
+            <h3>
+                titolo-originale: {{titoloOriginal}}
+            </h3>
+            <h3>
+                lingua: {{lingua}}
+            </h3>
+            <h3>
+                voto: {{voto}}
+            </h3>
+        </li>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
--- src/components/MainApp.vue
+++ src/components/MainApp.vue
@@ -1,7 +1,11 @@
 <script>
 import {store} from '../data/store';
+import DynamicLi from './DynamicLi.vue';
 export default {
     name:'MainApp',
+    components: {
+        DynamicLi
+    },
     data(){
         return {
             store
@@ -15,44 +19,28 @@ export default {
    <main>
   <div v-if="store.isSearch">
     <h3>
-        FILMS
+        films
     </h3>
     <ul>
-        <li v-for="(films, index) in store.arrayFilms" :key="index">
-            <h3>
-                titolo: {{films.title}}
-            </h3>
-            <h3>
-                titolo-originale: {{films.original_title}}
-            </h3>
-            <h3>
-                lingua: {{films.original_language}}
-            </h3>
-            <h3>
-                voto: {{films.vote_average}}
-            </h3>
-        </li>
+        <DynamicLi 
+        v-for="(films, index) in store.arrayFilms" :key="index" 
+        :titolo="films.title" 
+        :titoloOriginal="films.original_title"
+        :lingua="films.original_language"
+        :voto="films.vote_average"/>
     </ul>
   </div>
     <div v-if="store.isSearch">
         <h3>
-            serie tv
-        </h3>
-        <ul>
-        <li v-for="(serieTv, index) in store.arraySerieTv" :key="index">
-            <h3>
-                titolo: {{serieTv.name}}
-            </h3>
-            <h3>
-                titolo-originale: {{serieTv.original_name}}
-            </h3>
-            <h3>
-                lingua: {{serieTv.original_language}}
-            </h3>
-            <h3>
-                voto: {{serieTv.vote_average}}
-            </h3>
-        </li>
+        serie tv
+    </h3>
+    <ul>
+        <DynamicLi 
+        v-for="(serieTv, index) in store.arraySerieTv" :key="index" 
+        :titolo="serieTv.name" 
+        :titoloOriginal="serieTv.original_name"
+        :lingua="serieTv.original_language"
+        :voto="serieTv.vote_average"/>
     </ul>
     </div>
    </main>
