bae1d5de3158662f62ee2388f7bcef7c3460f429
MirkoFarina <MirkoFarina@outlook.com>
2022-11-21 15:01:23 +0100

    [add] milestone 2

--- src/components/DynamicLi.vue
+++ src/components/DynamicLi.vue
@@ -1,4 +1,5 @@
 <script>
+
 export default {
     name:'DynamicLi',
     props: {
@@ -6,6 +7,32 @@ export default {
         titoloOriginal: String,
         lingua: String,
         voto: Number
+    },
+    data(){
+        return {
+            lenguageNotPresents: [
+                'ja',
+                'ko',
+                'zh'
+            ]
+        }
+    },
+    methods: {
+
+    },
+    computed: {
+        getClassName(){
+            if(this.lingua === 'en') {
+                return 'fi fi-' + 'gb';
+            }else {
+                return 'fi fi-' + this.lingua;
+            }
+        },
+        lenguageNotFound(){
+            if(this.lenguageNotPresents.includes(this.lingua)){
+                return 'NOT FOUND'
+            }
+        }
     }
 }
 </script>
@@ -19,8 +46,11 @@ export default {
             <h3>
                 titolo-originale: {{titoloOriginal}}
             </h3>
-            <h3>
-                lingua: {{lingua}}
+            <h3 >
+                lingua: 
+                <span :class="getClassName">
+                    {{lenguageNotFound}}
+                </span> 
             </h3>
             <h3>
                 voto: {{voto}}
@@ -29,5 +59,4 @@ export default {
 </template>
 
 <style lang="scss" scoped>
-
 </style>
